63ee387a4e023bcb5b0253a1a02c64ee3e927c37
An <aha181@sfu.ca>
2025-10-30 18:58:34 -0700

    fixed not being able to see tanks_data

--- cogs/war_thunder.py
+++ cogs/war_thunder.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-from tanks_data import *
+from .tanks_data import *
 
 class war_thunder(commands.Cog):
     """War Thunder specific commands"""
